cf7535867cf37768a0b39659b225f795bfdf893a
disturbedhha <hughdarker.ins@gmail.com>
2023-03-14 23:55:14 -0500

    first

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "matrix.h"
+
+
+/*
+Create a function matrix
+Create a function to print the matrix
+create a funciton to transpose the matrix
+create a function to print the matrix in a file
+create a function to read the numbers of a text and print the numbers
+
+later create function to make basis operation between to arrays(matrix)
+use a pointer to select each case
+when the program is over create a library
+
+*/
+int main()
+{
+
+    std::cout << "hello world";
+
+    return 0;
+}
